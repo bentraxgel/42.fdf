bf32493eeea195727ecd3e1de8c7b6562e9cade1
Sewon Ok <seok@student.42seoul.kr>
2023-06-13 19:28:53 +0900

    for문으로 그리드 만들기 ok

--- Makefile
+++ Makefile
@@ -1,15 +1,21 @@
 CC = cc
-CFLAG = -Wall -Wextra -Werrer -Imlx
+CFLAG = -Wall -Wextra -Werror -Imlx
 NAME = test
 SRC = main.c
-OBJ = $(SRC:.c=.o)
+OBJ = $(SRC:%.c=%.o)
+OBJ_DIR = obj
+OBJ_FILE = $(addprefix $(OBJ_DIR)/, $(OBJ))
 
-$(NAME): $(OBJ)
-	$(CC) $(OBJ) -Lminilibx -lmlx -framework OpenGL -framework AppKit -o $(NAME)
+$(NAME): $(OBJ_FILE)
+	$(CC) $(CFLAG) -Lminilibx -lmlx -framework OpenGL -framework AppKit -o $(NAME) $(OBJ_FILE)
+$(OBJ_DIR) : 
+	mkdir -p $(OBJ_DIR)
+$(OBJ_DIR)/%.o : %.c | $(OBJ_DIR)
+	$(CC) $(CFLAG) -c $< -o $@
 
 all : $(NAME)
 clean : 
-	rm -rf $(OBJS)
+	rm -rf $(OBJ_DIR)
 fclean : clean
 	rm -rf $(NAME)
 re : fclean all
--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <unistd.h>
-#include <mlx.h>
+#include "mlx.h"
 
 //이미지의 정보를 나타내는 변수를 저장한 구조체
 typedef struct s_data
@@ -27,24 +27,71 @@ int main()
 	t_data	image;
 
 	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "Hellow World!");
-	image.img = mlx_new_image(mlx_ptr, 500, 500); // 이미지 객체 생성
+	win_ptr = mlx_new_window(mlx_ptr, 1000, 1000, "Hellow World!");
+	image.img = mlx_new_image(mlx_ptr, 800, 800); // 이미지 객체 생성
 	image.addr = mlx_get_data_addr(image.img, &image.bits_per_pixel, &image.line_length, &image.endian); // 이미지 주소 할당
-	for (int i = 0 ; i < 500 ; i++)
+	for (int i = 0 ; i < 800 ; i++)
 	{
 		for (int j = 0 ; j < 400 ; j++)
 		{
-			mlx_pixel_put (mlx_ptr, win_ptr, i, j, 0x00FFFFFF);
-			my_mlx_pixel_put(&image, i, j, 0x0000000F);
+			// mlx_pixel_put (mlx_ptr, win_ptr, i, j, 0x00FFFFFF);
+			my_mlx_pixel_put(&image, i, j, 0xFF99CC);
 		}	
 	}
-	my_mlx_pixel_put(&image, 100, 100, 0x00FFFFFF);
-	my_mlx_pixel_put(&image, 101, 101, 0x00FFFFFF);
-	my_mlx_pixel_put(&image, 102, 102, 0x00FFFFFF);
-	mlx_pixel_put(mlx_ptr, win_ptr, 100, 100, 0x00FFFFFF);
-	mlx_pixel_put(mlx_ptr, win_ptr, 101, 99, 0x00FFFFFF);
-	mlx_pixel_put(mlx_ptr, win_ptr, 102, 98, 0x00FFFFFF);
-	mlx_pixel_put(mlx_ptr, win_ptr, 103, 97, 0x00FFFFFF);
+	for (int i = 100; i < 200; i++)
+	{
+		for (int j = 100; j < 200; j++)
+			my_mlx_pixel_put(&image, i, j, 0x000000);
+	}
+	for (int i = 300; i < 500; i++)
+		my_mlx_pixel_put(&image, i, 200, 0xFFFFFF);
+	for (int i = 300; i < 500; i += 10)
+	{
+		my_mlx_pixel_put(&image, i, 200, 0x3399FF);
+		my_mlx_pixel_put(&image, i + 1, 200, 0x3399FF);
+		my_mlx_pixel_put(&image, i + 2, 200, 0x3399FF);
+	}
+	for (int i = 300; i < 500; i++)
+		my_mlx_pixel_put(&image, i, 250, 0xFFFFFF);
+	for (int i = 300; i < 500; i += 10)
+	{
+		my_mlx_pixel_put(&image, i, 250, 0x3399FF);
+		my_mlx_pixel_put(&image, i + 1, 250, 0x3399FF);
+		my_mlx_pixel_put(&image, i + 2, 250, 0x3399FF);
+	}
+	for (int i = 300; i < 500; i++)
+		my_mlx_pixel_put(&image, i, 300, 0xFFFFFF);
+	for (int i = 300; i < 500; i += 10)
+	{
+		my_mlx_pixel_put(&image, i, 300, 0x3399FF);
+		my_mlx_pixel_put(&image, i + 1, 300, 0x3399FF);
+		my_mlx_pixel_put(&image, i + 2, 300, 0x3399FF);
+	}
+	for (int row = 200; row <= 300; row += 50)
+	{
+		for (int col = 300; col <= 500; col++)
+			my_mlx_pixel_put(&image, col, row, 0xFFFFFF);
+		for (int col = 300; col <= 500; col += 10)
+		{
+			my_mlx_pixel_put(&image, col, row, 0x3388FF);
+			my_mlx_pixel_put(&image, col, row + 1, 0x3388FF);
+			my_mlx_pixel_put(&image, col, row + 1, 0x3388FF);
+		}
+	}
+	for (int col = 300; col <= 500; col += 50)
+	{
+		for (int row = 200; row < 300; row++)
+			my_mlx_pixel_put(&image, col, row, 0xFFFFFF);
+		for (int row = 200; row < 300; row += 10)
+		{
+			my_mlx_pixel_put(&image, col, row, 0x3399FF);
+			my_mlx_pixel_put(&image, col, row + 1, 0x3399FF);
+			my_mlx_pixel_put(&image, col, row + 2, 0x3399FF);
+		}
+	}
+	// my_mlx_pixel_put(&image, -500, -500, 0xFFFFFF);
+	// my_mlx_pixel_put(&image, -501, -501, 0xFFFFFF);
+	// my_mlx_pixel_put(&image, -502, -502, 0xFFFFFF);
 	mlx_put_image_to_window(mlx_ptr, win_ptr, image.img, 10, 100);
 	mlx_loop(mlx_ptr);
 	return (0);
